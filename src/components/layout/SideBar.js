d039267d3e5936a6f41ca0518b9f09587ad9add4
Sharofiddin Azizmatov <azizmatov1202@gmail.com>
2022-03-19 01:38:02 +0200

    creating basic layout

---
+++ src/components/layout/SideBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SideBar = () => {
+  return (
+    <div className='sidebar-container'>
+      <div className='logo'>
+        <img src='./img/logo.svg' alt='' />
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
