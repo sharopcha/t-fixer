df86aa90d4eeb37ab408b9760c434530a8440c17
Sharofiddin Azizmatov <azizmatov1202@gmail.com>
2022-03-19 19:44:44 +0200

    dialog tested in panel

--- src/components/layout/Panel.js
+++ src/components/layout/Panel.js
@@ -1,9 +1,35 @@
 import { Button } from 'primereact/button';
-import React from 'react';
+import React, { useState } from 'react';
 import Circle from '../Circle';
 import FullDay from '../FullDay';
+import { Dialog } from 'primereact/dialog';
 
 const Panel = () => {
+  const [displayMaximizable, setDisplayMaximizable] = useState(false);
+
+  const onClick = () => setDisplayMaximizable(true);
+
+  const onHide = () => setDisplayMaximizable(false);
+
+  const renderFooter = () => {
+    return (
+      <div>
+        <Button
+          label='No'
+          icon='pi pi-times'
+          onClick={() => onHide()}
+          className='p-button-text'
+        />
+        <Button
+          label='Yes'
+          icon='pi pi-check'
+          onClick={() => onHide()}
+          autoFocus
+        />
+      </div>
+    );
+  };
+
   return (
     <div className='panel'>
       <div>
@@ -17,6 +43,7 @@ const Panel = () => {
         <div>
           <p>Tasks</p>
           <Button
+            onClick={() => onClick()}
             label='New Task'
             className='p-button-raised p-button-success btn'
           />
@@ -27,6 +54,26 @@ const Panel = () => {
         <p>Upcoming Exams</p>
         <Circle labelTitle={'Exams'} />
       </div>
+
+      <Dialog
+        header='Header'
+        visible={displayMaximizable}
+        maximizable
+        modal
+        style={{ width: '50vw' }}
+        footer={renderFooter()}
+        onHide={() => onHide()}
+      >
+        <p className='m-0'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </Dialog>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
 ReactDOM.render(
   <React.StrictMode>
